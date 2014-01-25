3c7d342a780756b9d924234fc9be16409a7e3430
aac85 <alfonsoagud@hotmail.com>
2014-01-25 22:06:58 +0100

    el muñeco bota mejor

--- Assets/Scripts/Movement.js
+++ Assets/Scripts/Movement.js
@@ -1,16 +1,41 @@
+private var colision : boolean;
+private var x : int;
+private var y : int;
 
-//public var player : GameObject;
-//private var player : RaycastHit;
-function FixedUpdate ()
+function Start ()
 {
-//if(this.collider2D.tag == "platform"){
-	if(Input.GetKey (KeyCode.RightArrow))
-		this.rigidbody2D.velocity = new Vector2 (5.0, 0.0);	
+	colision = false;
+	x=0;
+	y=0;
+}
+
+function OnCollisionEnter2D(coll: Collision2D) 
+{
+	colision = true;
+}
+
+function FixedUpdate (){
+	Debug.Log(colision);
+		if(Input.GetKey (KeyCode.RightArrow) && Input.GetKey (KeyCode.UpArrow) && colision){
+			this.rigidbody2D.velocity = new Vector2 (7.0, 7.0);	
+			colision = false;
+		}
+		
+		if(Input.GetKey (KeyCode.LeftArrow) && Input.GetKey (KeyCode.UpArrow) && colision){
+			this.rigidbody2D.velocity = new Vector2 (-7.0, 7.0);	
+			colision = false;
+		}
+			
+		if(Input.GetKey (KeyCode.RightArrow) && colision)
+			this.rigidbody2D.velocity = new Vector2 (7.0, y);	
 	
-	if(Input.GetKey (KeyCode.LeftArrow))
-		this.rigidbody2D.velocity = new Vector2(-5.0, 0.0);	
+		if(Input.GetKey (KeyCode.LeftArrow) && colision)
+			this.rigidbody2D.velocity = new Vector2(-7.0, y);	
+			
+		if(Input.GetKey (KeyCode.UpArrow) && colision){
+			this.rigidbody2D.velocity = new Vector2(x, 7.0);
+			colision = false;
+		}
 	
-	if(Input.GetKey (KeyCode.UpArrow))
-		this.rigidbody2D.velocity = new Vector2(0.0, 5.0);	//add rotational speed to rear wheel
-//	}
+	
 }
