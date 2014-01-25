85fbfbcc5f0e4cf22e155cf11b39dacc981e0ed5
octal0 <octal0@gmail.com>
2014-01-26 00:31:41 +0100

    Cámara sigue al personaje

---
+++ Assets/Scripts/CameraBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraBehaviour : MonoBehaviour {
+
+	public GameObject target;
+	public float deltax;
+	public float deltay;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 pos = this.transform.position;
+		Vector3 targetpos = target.transform.position;
+
+		if(Mathf.Abs(pos.x - targetpos.x) > deltax)
+			//Mover la x hacia la posicion del target
+			pos.x = 0.90f * pos.x + 0.10f * targetpos.x;
+
+		if(Mathf.Abs(pos.y - targetpos.y) > deltay)
+			//Mover la x hacia la posicion del target
+			pos.y = 0.90f * pos.y + 0.10f * targetpos.y;
+				
+		this.transform.position = pos;
+
+	}
+}
