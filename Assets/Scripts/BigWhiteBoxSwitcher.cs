2c7232fedaee29995913913186f09ce482a5a5f5
octal0 <octal0@gmail.com>
2014-01-26 14:38:26 +0100

    Cajas Grande->Pequeña
    Texturas, prefabs y scripts

---
+++ Assets/Scripts/BigWhiteBoxSwitcher.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BigWhiteBoxSwitcher : MonoBehaviour, IFadeable {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void SetFadeState(float value) {
+
+		if(value < 0.1)
+		{
+			this.transform.localScale = new Vector3(1,1,1);
+			this.GetComponent<Rigidbody2D>().isKinematic = false;
+			this.GetComponent<BoxCollider2D>().size = new Vector2(1.0f,1.0f);
+			this.GetComponent<BoxCollider2D>().center = new Vector2(0.0f,0.0f);
+		}
+
+
+		if(value > 0.9)
+		{
+			this.transform.localScale = new Vector3(3,3,1);
+			this.GetComponent<Rigidbody2D>().isKinematic = true;
+			this.GetComponent<BoxCollider2D>().size = new Vector2(1.3f,2.2f);
+			this.GetComponent<BoxCollider2D>().center = new Vector2(0.0f,-0.6f);
+		}
+		
+
+	}
+}
